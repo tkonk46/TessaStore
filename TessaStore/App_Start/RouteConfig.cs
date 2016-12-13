5b85d7e559f8007c3f283ef670586e2a076aaa51
tkonk46 <tessa.konkol@gmail.com>
2016-12-13 12:07:51 -0600

    Added Students Route

--- TessaStore/App_Start/RouteConfig.cs
+++ TessaStore/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace TessaStore
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Students",
+                url: "students/{id}",
+                defaults: new { controller = "Students", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ TessaStore/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TessaStore.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students/{id}
+        public ActionResult Index(int? id)
+        {
+            if(id != null)
+            {
+                switch(id.Value)
+                {
+                    case 1:
+                        return Json(new { name = "Jimmy Ellis" }, JsonRequestBehavior.AllowGet);
+                    case 2:
+                        return Json(new { name = "Jin Xiao" }, JsonRequestBehavior.AllowGet);
+                    case 3:
+                        return Json(new { name = "Tessa Konkol" }, JsonRequestBehavior.AllowGet);
+                    case 4:
+                        return Json(new { name = "Serkan Nizam" }, JsonRequestBehavior.AllowGet);
+                    case 5:
+                        return Json(new { name = "Jerry Bony" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return HttpNotFound("No student found");
+        }
+    }
+}
